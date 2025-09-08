6cec54a882c0a42a4db2d80446aaea63e9d3fd36
Soumik Ghosh <115470141+Soumik-Ghosh-2924@users.noreply.github.com>
2025-09-08 20:13:55 +0530

    Refactored the Course-Controller class as well as the main driver class for the application as per a standard application.

--- src/main/java/io/javabrains/CourseApiDataApplication.java
+++ src/main/java/io/javabrains/CourseApiDataApplication.java
@@ -1,22 +1,20 @@
 package io.javabrains;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.logging.Logger;
-
-
 @SpringBootApplication
 public class CourseApiDataApplication 
 {
-	private static final Logger logger = Logger.getLogger(CourseApiDataApplication.class.getName());
-	public static void main(String[] args) 
-	{
-		logger.info("Starting the Learning-Management-System application .");
-		SpringApplication.run(CourseApiDataApplication.class, args);
-
-		logger.info("Application is up and running...!");
-
-	}
-
+	private static final Logger logger = LoggerFactory.getLogger(CourseApiDataApplication.class.getName());
+	public static void main(String[] args) {
+        try {
+            logger.info("Starting the Learning-Management-System application .");
+            SpringApplication.run(CourseApiDataApplication.class, args);
+            logger.info("Application is up and running...!");
+        } catch (Exception e) {
+			logger.error("Failure in starting the application : ");
+            throw new RuntimeException(e);
+        }
+    }
 }
